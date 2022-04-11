2411095aa5f5c4a7abf1004c90d9d588d0207939
Calvine Otieno <nyarangaotieno@gmail.com>
2022-04-11 17:07:03 +0300

    chore: add ip to allowed_hosts
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- app/app/settings.py
+++ app/app/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+from socket import gethostname, gethostbyname
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -33,6 +35,9 @@ ALLOWED_HOSTS.extend(
         os.environ.get('ALLOWED_HOSTS', '').split(',')
     )
 )
+
+if os.environ.get('AWS_EXECUTION_ENV'):
+    ALLOWED_HOSTS.append(gethostbyname(gethostname()))
 
 
 # Application definition
